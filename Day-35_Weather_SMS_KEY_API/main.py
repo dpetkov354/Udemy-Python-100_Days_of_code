f20cfea39f7973f5764b2db895e36107c4b93ffd
Dimitar Petkov <103525420+dpetkov354@users.noreply.github.com>
2022-06-30 16:38:43 +0300

    Update main.py

--- Day-35_Weather_SMS_KEY_API/main.py
+++ Day-35_Weather_SMS_KEY_API/main.py
@@ -10,18 +10,18 @@ rainy = False
 
 # WEATHER PAI INFO
 OWM_END_POINT = "https://api.openweathermap.org/data/2.5/onecall?"
-api_key = "972f39b8d81423413d625e377b079917"
+api_key = 'INSERT HERE'
 
 # TWILIO SMS API INFO
-account_sid = 'ACbab9fb39ac43b345cd6950dbd19731ed'
-auth_token = '7197c3a0daa2ab6f7ec67cc9d5eb23fe'
+account_sid = 'INSERT HERE'
+auth_token = 'INSERT HERE'
 
 # API PARAMETERS
 
 params = {
     "lat": 42.430894,
     "lon": 25.622741,
-    "appid": "972f39b8d81423413d625e377b079917",
+    "appid": 'INSERT HERE',
     "exclude": "current,minutely,daily"
 }
 
@@ -48,8 +48,8 @@ if rainy:
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         body="From 06:00 to 18:00 it might rain, so bring an umbrella!☔",
-        from_='+18647635176',
-        to='+359896877082'
+        from_='PHONE NUMBER HERE',
+        to='PHONE NUMBER HERE'
     )
     print(message.status)
 
@@ -68,18 +68,18 @@ if rainy:
 #
 # # WEATHER PAI INFO
 # OWM_END_POINT = "https://api.openweathermap.org/data/2.5/onecall?"
-# api_key = "972f39b8d81423413d625e377b079917"
+# api_key = 'INSERT HERE'
 #
 # # TWILIO SMS API INFO
-# account_sid = 'ACbab9fb39ac43b345cd6950dbd19731ed'
-# auth_token = '7197c3a0daa2ab6f7ec67cc9d5eb23fe'
+# account_sid = 'INSERT HERE'
+# auth_token = 'INSERT HERE'
 #
 # # API PARAMETERS
 #
 # params = {
 #     "lat": 42.430894,
 #     "lon": 25.622741,
-#     "appid": "972f39b8d81423413d625e377b079917",
+#     "appid": 'INSERT HERE',
 #     "exclude": "current,minutely,daily"
 # }
 #
